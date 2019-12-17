f6b35369db1f39d78f6c99e4cd4b188b992fc4cf
Thomas <thomas.foch@outlook.fr>
2019-12-17 22:57:06 +0100

    Formulaire et verification -> in progress

---
+++ src/theater_forms/createPestacleForm.java
@@ -0,0 +1,106 @@
+package theater_forms;
+
+import theater_beans.pestacle;
+
+import java.util.HashMap;
+import java.util.Map;
+import javax.servlet.http.HttpServletRequest;
+
+public final class createPestacleForm {
+
+    private static final String FIELD_NAME = "name";
+    private static final String FIELD_CATEGORY = "category";
+    private static final String FIELD_DATE = "date";
+    private static final String FIELD_PLACE = "place";
+
+    private String result;
+    private Map<String, String> error = new HashMap<String, String>();
+
+    public String getResult() {
+        return result;
+    }
+
+    public Map<String, String> getErreurs() {
+        return error;
+    }
+
+    public pestacle create(HttpServletRequest request) {
+        String name = getValueField(request, FIELD_NAME);
+        String category = getValueField(request, FIELD_CATEGORY);
+        String date = getValueField(request, FIELD_DATE);
+        String place = getValueField(request, FIELD_PLACE);
+
+        pestacle pestacle = new pestacle();
+
+        try {
+            validateString(name);
+        } catch (Exception e) {
+            setError(FIELD_NAME, e.getMessage());
+        }
+        pestacle.setName(name);
+
+        try {
+            validateString(category);
+        } catch (Exception e) {
+            setError(FIELD_CATEGORY, e.getMessage());
+            System.out.println(getErreurs());
+            System.out.println(error.get("name"));
+        }
+        pestacle.setName(category);
+
+/*
+        try {
+            validateSale( place );
+        } catch ( Exception e ) {
+            setErreur( FIELD_PLACE, e.getMessage() );
+        }
+        pestacle.setPlace( place );
+*/
+
+        if (error.isEmpty()) {
+            result = "Succès de la création du pestacle.";
+        } else {
+            result = "Échec de la création du pestacle.";
+        }
+
+        return pestacle;
+    }
+
+    private void validateString(String name) throws Exception {
+        if (name != null) {
+            if (!name.matches("([^.@]+)(\\.[^.@]+)*@([^.@]+\\.)+([^.@]+)")) {
+                throw new Exception("Merci de saisir un nom de pestacle valide.");
+            }
+        } else {
+            throw new Exception("Le champ ne ne peut pas être vide.");
+        }
+    }
+
+    private void validateSale(Integer place) throws Exception {
+        if (place == 0) {
+            throw new Exception("Le specatcle doit au moins pouvoir accueillir un spectateur.");
+        }
+    }
+
+    /*
+     * Ajoute un message correspondant au champ spécifié à la map des erreurs.
+     */
+    private void setError(String field, String message) {
+        error.put(field, message);
+    }
+
+    /*
+     * Méthode utilitaire qui retourne null si un champ est vide, et son contenu
+     * sinon.
+     */
+    private static String getValueField(HttpServletRequest request, Object fieldValue) {
+
+        String valeur = request.getParameter((String) fieldValue);
+        if (valeur == null || valeur.trim().length() == 0) {
+            return null;
+        } else {
+            return valeur.trim();
+        }
+    }
+}
+
---
+++ web/WEB-INF/vue/form.jsp
@@ -0,0 +1,43 @@
+<%@ page pageEncoding="UTF-8" %>
+<%@ taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c" %>
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8" />
+    <title>Creation</title>
+    <link type="text/css" rel="stylesheet" href="" />
+</head>
+<body>
+<form method="post" action="createPestacle">
+    <fieldset>
+        <legend>Création d'un pestacle</legend>
+        <p>Vous pouvez créer un pestacle via ce formulaire.</p>
+
+        <label for="name">Nom du pestacle <span class="requis">*</span></label>
+        <input type="text" id="name" name="name" value="<c:out value="${pestacle.name}"/>" size="20" maxlength="60" />
+        <span class="error">${form.error["name"]}</span>
+        <br />
+
+        <label for="category">Catégorie <span class="requis">*</span></label>
+        <input type="text" id="category" name="category" value="<c:out value="${pestacle.category}"/>" size="20" maxlength="60" />
+        <span class="error">${form.error['category']}</span>
+        <br />
+
+        <label for="date">Date <span class="requis">*</span></label>
+        <input type="Date" id="date" name="date" value="" size="20" maxlength="60" />
+        <span class="error">${form.error['date']}</span>
+        <br />
+
+        <label for="place">Nombre de place</label>
+        <input type="text" id="place" name="place" value="<c:out value="${pestacle.place}"/>" size="20" maxlength="20" />
+        <span class="error">${form.error['place']}</span>
+        <br />
+
+        <input type="submit" value="Créer" class="sansLabel" />
+        <br />
+
+        <p class="${empty form.error ? 'succes' : 'error'}">${form.result}</p>
+    </fieldset>
+</form>
+</body>
+</html>
